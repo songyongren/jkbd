ef635c4de20bf1ba0a80936cc207562bf73f6d8f
songyongren <1193390646@qq.com>
2017-06-30 16:36:05 +0800

    网络获取题目，Picasso拉取图片

--- app/src/main/java/com/example/administrator/myapplication/ExamApplication.java
+++ app/src/main/java/com/example/administrator/myapplication/ExamApplication.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.example.administrator.myapplication.bean.Examlnfo;
 import com.example.administrator.myapplication.bean.*;
 import com.example.administrator.myapplication.utils.OkHttpUtils;
+import com.example.administrator.myapplication.utils.ResultUtils;
 
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class ExamApplication extends Application{
         initData();
     }
     public static ExamApplication getInstance(){
+
         return instance;
     }
 
@@ -49,9 +51,31 @@ public class ExamApplication extends Application{
 
                     @Override
                     public void onError(String error) {
+
                         Log.e("main","error"+error);
                     }
                 });
+                OkHttpUtils<String>utils1=new OkHttpUtils<String>(instance);
+                String url2 = "http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
+                utils1.url(url2)
+                        .targetClass(String.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<String>() {
+                            @Override
+                            public void onSuccess(String result) {
+                               Result result1= ResultUtils.getListResultFromJson(result);
+                               if(result1!=null&&result1.getError_code()==0){
+                                   List<Question>list =result1.getResult();
+                                   if(list!=null&&list.size()>0){
+                                       mExamList=list;
+                                   }
+                               }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
             }
         }).start();
     }
--- app/src/main/java/com/example/administrator/myapplication/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/ExamActivity.java
@@ -2,18 +2,24 @@ package com.example.administrator.myapplication.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.myapplication.ExamApplication;
 import com.example.administrator.myapplication.R;
 import com.example.administrator.myapplication.bean.Examlnfo;
+import com.example.administrator.myapplication.bean.Question;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/6/29.
  */
 
 public class ExamActivity extends AppCompatActivity{
-    TextView tvExamInfo;
+    TextView tvExamInfo,tvExamTitle,tvOp1,tvOp2,tvOp3,tvOp4;
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,12 @@ public class ExamActivity extends AppCompatActivity{
     private void initView() {
 
         tvExamInfo=(TextView)findViewById(R.id.tv_examinfo);
+        tvExamTitle=(TextView)findViewById(R.id.exam_title);
+        tvOp1=(TextView)findViewById(R.id.tv_op1);
+        tvOp2=(TextView)findViewById(R.id.tv_op2);
+        tvOp3=(TextView)findViewById(R.id.tv_op3);
+        tvOp4=(TextView)findViewById(R.id.tv_op4);
+        mImageView=(ImageView)findViewById(R.id.im_exam_imge);
     }
 
     private void initData(){
@@ -33,9 +45,29 @@ public class ExamActivity extends AppCompatActivity{
         if(examInfo!=null){
             showData(examInfo);
         }
+        List<Question> Queslist=ExamApplication.getInstance().getmExamList();
+        if(Queslist!=null){
+               showExam(Queslist);
+
+        }
+    }
+
+    private void showExam(List<Question> queslist) {
+        Question question=queslist.get(0);
+        if(question!=null){
+            tvExamTitle.setText(question.getQuestion());
+            tvOp1.setText(question.getItem1());
+            tvOp2.setText(question.getItem2());
+            tvOp3.setText(question.getItem3());
+            tvOp4.setText(question.getItem4());
+            Picasso.with(ExamActivity.this)
+                    .load(question.getUrl())
+                    .into(mImageView);
+        }
     }
 
     private void showData(Examlnfo examInfo) {
+
         tvExamInfo.setText(examInfo.toString());
     }
 
